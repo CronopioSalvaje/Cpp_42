5f061c16ab47701bfdb038afa91fbfd7e57fc51b
chris <chrisap@live.fr>
2024-09-19 12:30:32 +0200

    corrected and improved main ex03

--- module03/ex03/main.cpp
+++ module03/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: calbor-p <calbor-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 08:16:26 by calbor-p          #+#    #+#             */
-/*   Updated: 2024/09/19 12:15:15 by calbor-p         ###   ########.fr       */
+/*   Updated: 2024/09/19 12:27:55 by calbor-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,40 @@
 #include "FragTrap.hpp"
 #include "DiamondTrap.hpp"
 
-#define line std::cout << std::endl;
-#define linex2 line line
+#define line std::cout << std::endl
+#define linex2 line; line
 
 int main( void )
 {
-
-   std::cout << "DiamondTrap tests ____________" << std::endl << std::endl;
+   std::cout << "Creation : "; line;
+   std::cout << "-----------"; line;
    DiamondTrap maceo("Maceo");   
+   DiamondTrap maceo2(maceo);
+   DiamondTrap maceo3 = maceo;
+   line;
+   std::cout << "DiamondTrap tests ____________" << std::endl << std::endl;
    maceo.whoAmI();
    maceo.attack("Conan");
-   line
-   DiamondTrap maceo2(maceo);
+   maceo.attack("Conan");
+   line;
    maceo2.whoAmI();
    maceo2.attack("Conan");
-   line   
-   DiamondTrap maceo3 = maceo;
+   line  ;
    maceo3.whoAmI();
-   maceo3.attack("Conan");
-   line
+   maceo3.guardGate();
+   maceo3.highFivesGuys();
+   line;
+
+    std::cout << "Maceo : energy points : " << maceo.getEnergyPoints()
+   << " - hit points : " << maceo.getHitPoints() << std::endl;
+    std::cout << "Maceo2 : energy points : " << maceo2.getEnergyPoints()
+   << " - hit points : " << maceo2.getHitPoints() << std::endl;
+    std::cout << "Maceo3 : energy points : " << maceo3.getEnergyPoints()
+   << " - hit points : " << maceo3.getHitPoints() << std::endl;
+   
    std::cout << "DiamondTrap tests end____________" << std::endl << std::endl;
    
+   line;
    return 0;
 }
 
