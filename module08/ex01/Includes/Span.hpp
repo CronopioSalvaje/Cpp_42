1d38c80681ae14491c3a303a6e43ad0dcf2675c4
CronopioSalvaje <chrisap@live.fr>
2024-10-31 15:46:46 +0100

    ex01

---
+++ module08/ex01/Includes/Span.hpp
@@ -0,0 +1,53 @@
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+#include <iostream>
+
+template<typename T>
+class Span
+{
+    private:
+        unsigned int size;
+        T* data;
+    public:
+        Span(unsigned int size):size(size)
+        {
+            data = new T[size];
+        }
+
+        Span(Span const &sp)
+        {
+            *this = sp;
+        }
+
+        T &operator[](size_t index)
+        {
+            return data[index];
+        }
+
+        Span &operator=(Span const &sp)
+        {
+            if (this != &sp)
+            {
+                size = sp.size;
+                if (data)
+                    delete[] data;
+                data = new T[size];
+                for (size_t i = 0; i < size; ++i)
+                    data[i] = sp.data[i];
+            }
+            return *this;
+        }
+        void addNumber(unsigned int value);
+        size_t shortestSpan();
+        size_t longestSpan();
+        ~Span()
+        {
+            delete [] data;
+        };
+
+};
+
+#include "Span.tpp"
+
+#endif
