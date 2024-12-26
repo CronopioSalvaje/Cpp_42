b329470743ef3c2f9b1c6fe418c1d524e3a6f0ad
CronopioSalvaje <chrisap@live.fr>
2024-12-26 12:01:43 +0100

    parsing almost finished

--- module09/ex00/Includes/BitcoinExchange.hpp
+++ module09/ex00/Includes/BitcoinExchange.hpp
@@ -7,18 +7,36 @@
 #include <sstream>
 #include "cstdlib"
 #include <map>
-
+#define NO_LIMIT 0
+#define LIMIT 1000
 #define DB_PATH "Includes/data.csv"
+#define DATABASE 0
+#define USERFILE 1000
+
+typedef struct s_date {
+    int year;
+    int month;
+    int day;
+    std::string d_str;
+    bool operator<(const s_date& other) const {
+        if (year != other.year)
+            return year < other.year;
+        if (month != other.month)
+            return month < other.month;
+        return day < other.day;
+    }
+} t_date;
 
 class BitcoinExchange
 {
     private:
-        std::map<std::string, float> m_btcExchange;
-        std::map<std::string, float> m_DB;
+        std::map<t_date, double> m_btcExchange;
+        std::map<t_date, double> m_DB;
         BitcoinExchange(BitcoinExchange const &cl);
         BitcoinExchange &operator=(BitcoinExchange const &cl);
-        void parseline(std::string &line, std::map<std::string, float> map);
+        void parseline(std::string &line, std::map<t_date, double> map, int ctx);
         void parseDatabase();
+        void parseUserfile(std::string path);
     public:
         BitcoinExchange(std::string const &path);
         ~BitcoinExchange();
--- module09/ex00/Srcs/BitcoinExchange.cpp
+++ module09/ex00/Srcs/BitcoinExchange.cpp
@@ -15,7 +15,7 @@ bool onlyDigits(const char *str)
     return true;
 }
 
-bool isValidInRange(std::string nbr, int min, int max)
+bool isValidInRange(std::string nbr, int min, int max, t_date *d)
 {
     std::stringstream ss;
     int number;
@@ -25,23 +25,29 @@ bool isValidInRange(std::string nbr, int min, int max)
     ss >> number;
     if (ss.fail())
         return false;
+    switch (max)
+    {
+        case 12: d->month = number; break;
+        case 31: d->day = number; break;    
+        default: d->year = number; break;
+    }
     return (number >= min && number <= max);
 }
 
-bool isValidDay(std::string date){
+bool isValidDay(std::string date, t_date *d){
     std::string day = date.substr(8,2);
-    return isValidInRange(day, 1, 31);
+    return isValidInRange(day, 1, 31, d);
 }
 
-bool isValidMonth(std::string date){
+bool isValidMonth(std::string date, t_date *d){
     std::string month = date.substr(5,2);
-    return isValidInRange(month, 1, 12);
+    return isValidInRange(month, 1, 12, d);
 }
 
-bool isValidYear(std::string date)
+bool isValidYear(std::string date, t_date *d)
 {
     std::string year = date.substr(0,4);
-    return isValidInRange(year, 2000, 2100);
+    return isValidInRange(year, 2000, 2100, d);
 }
 
 bool isValidSize(std::string date)
@@ -56,23 +62,76 @@ bool areSeparatorsValid(std::string date)
     return (date[4] == '-' && date[7] == '-');
 }
 
-bool checkDateFormat(std::string date)
+bool checkDateFormat(std::string date, t_date *d)
 {
-    return isValidSize(date) && areSeparatorsValid(date) && isValidYear(date) && isValidMonth(date) && isValidDay(date);
+    return isValidSize(date) && areSeparatorsValid(date) && isValidYear(date, d) && isValidMonth(date, d) && isValidDay(date, d);
 }
 
-void BitcoinExchange::parseline(std::string &line, std::map<std::string, float> map)
+std::string trim(std::string str)
 {
-    size_t pos = line.find(",");
+    size_t first = str.find_first_not_of(" \t\n\r\f\v");
+    size_t last =  str.find_last_not_of(" \t\n\r\f\v");
+    
+    if (first == std::string::npos)
+        return "";
+    return (str.substr(first, last - first + 1));
+}
+
+bool checkValue(std::string value, double *val, int limit)
+{
+    std::stringstream ss;
+    ss << value;
+    double d;
+    ss >> d;
+    if (ss.fail())
+        throw GenericException("Error: Bad Input");
+        return false;
+    if (d < 0)
+    {
+        throw GenericException("Error: not a positive number");
+        return false;
+    }
+    if (limit == USERFILE && d > 1000)
+    {
+        throw GenericException("Error: number too large");
+        return false;
+    }
+    *val = d;
+    return true;        
+}
+
+void BitcoinExchange::parseline(std::string &line, std::map<t_date, double> map, int ctx)
+{    
+    size_t pos;
+    double val;
+    switch (ctx)
+    {
+    case 0: pos = line.find(","); break;
+    case 1000: pos = line.find("|"); break;
+    default:
+        break;
+    }     
     static int lnumb = 1;
-    std::string date = line.substr(0, pos);
-    if(!checkDateFormat(date))
+    std::string date = trim(line.substr(0, pos));
+    t_date d;
+    d.d_str = date;
+    if(!checkDateFormat(date, &d))
+    {
         std::cerr << "Error : bad format at line " << lnumb << std::endl;
+        lnumb++;
+        return;
+    }
     lnumb++;
     std::string value = "";
     if (pos !=std::string::npos)
-        value = line.substr(pos + 1, std::string::npos);
-    std::cout << BOLD_BLUE << date << "::" << value << std::endl;
+        value = trim(line.substr(pos + 1, std::string::npos));
+    if(!checkValue(value, &val, ctx))
+    {
+        std::cerr << "Error : bad format at line " << lnumb << std::endl;
+        lnumb++;
+        return;
+    }
+    map.insert(std::make_pair(d, val));
 }
 
 void BitcoinExchange::parseDatabase()
@@ -83,21 +142,26 @@ void BitcoinExchange::parseDatabase()
     std::string line;
     while (std::getline(file, line))
     {
-        parseline(line, m_DB);
+        parseline(line, m_DB, DATABASE);
     }
 }
 
-BitcoinExchange::BitcoinExchange(std::string const &path)
-{
-    parseDatabase();
+ void BitcoinExchange::parseUserfile(std::string path)
+ {
     std::ifstream file(path.c_str());
     if (!file || !file.is_open() || file.fail() || file.bad())
         throw FileErrorException("File Error");
     std::string line;
     while (std::getline(file, line))
     {
-        parseline(line, m_btcExchange);
+        parseline(line, m_btcExchange, USERFILE);
     }
+ }
+
+BitcoinExchange::BitcoinExchange(std::string const &path)
+{
+    parseDatabase();
+    parseUserfile(path);  
 }
 
 BitcoinExchange::~BitcoinExchange(){}
